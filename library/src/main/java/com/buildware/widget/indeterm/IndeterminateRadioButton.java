e116f6d25f405baa1094f257f410948ce3004459
Luca Weiss <luca@z3ntu.xyz>
2019-11-07 11:16:35 +0100

    Migrate to AndroidX

--- library/src/main/java/com/buildware/widget/indeterm/IndeterminateRadioButton.java
+++ library/src/main/java/com/buildware/widget/indeterm/IndeterminateRadioButton.java
@@ -3,8 +3,8 @@ package com.buildware.widget.indeterm;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Parcelable;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.AppCompatRadioButton;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatRadioButton;
 import android.util.AttributeSet;
 import android.view.ViewDebug;
 
